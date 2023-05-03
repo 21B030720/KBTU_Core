4a04e895775950bc7cf847d2e92062de24c4391b
21B030720 <jacklol5760@gmail.com>
2023-05-03 20:46:07 +0600

    create: simple serializers by requirements, icons for faculty

--- back/kbtu_core_back/api/views.py
+++ back/kbtu_core_back/api/views.py
@@ -3,21 +3,14 @@ from rest_framework import serializers
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 import json
 
 from api.models import Tutorial
 from api.serializers import *
 
 
-# class TutorialAll(generics.ListCreateAPIView):
-#     queryset = Tutorial.objects.all()
-#     serializer_class = TutorialSerializer
-#
-#
-# class TutorialDetail(generics.RetrieveUpdateDestroyAPIView):
-#     queryset = Tutorial.objects.all()
-#     serializer_class = TutorialSerializer
+
 class FacultyClass(APIView):
     def get(self, request, format=None):
         companies = Faculty.objects.all()
@@ -49,6 +42,8 @@ class TutorialClass(APIView):
             return Response(serializer.data)
     def post(self, request, format=None, id = 0):
         data = json.loads(request.body)
+        # data1 = request.POST.get('category', None)
+        # print(data1)
         title = data.get('title', '')
         author = data.get('author', '')
         category = Category.objects.get(id = id)
@@ -58,21 +53,10 @@ class TutorialClass(APIView):
         tutorial = Tutorial.objects.create(title = title, author = author, category = category, img = img, like = like, content = content)
         serializer = TutorialSerializer(tutorial)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-    # def put(self, request, id, format=None):
-    #     company = Company.objects.get(id=id)
-    #     serializer = CompanySerializer(company, data = request.data)
-    #     if(serializer.is_valid()):
-    #         serializer.save()
-    #         return JsonResponse(serializer.data, safe=False)
-    #     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    #
-    # def delete(self, request, id, format=None):
-    #     company = Tutorial.objects.get(id=id)
-    #     company.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
 def a1(request, id):
     faculty = Faculty.objects.get(id = id)
     category = Category.objects.filter(faculty = faculty)
     tutorial = Tutorial.objects.filter(category__in = category)
     serializer = TutorialSerializer(tutorial, many=True)
     return JsonResponse(serializer.data, safe=False)
+
